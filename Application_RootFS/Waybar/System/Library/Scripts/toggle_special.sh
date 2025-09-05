5c425797245e4b07cd57714d0a91fdd7523132b4
Ascellayn <root.siriusb@gmail.com>
2025-09-05 17:08:52 +0200

    Waybar | Better bar handling & rename script & separate from Hyprllian

--- Application_RootFS/Waybar/System/Library/Scripts/toggle_special.sh
+++ Application_RootFS/Waybar/System/Library/Scripts/toggle_special.sh
@@ -5,9 +5,19 @@ WayPID=$(pgrep ^waybar)
 echo $WayPID
 
 if [ -z "$WayPID" ] && [ "$(hyprctl activewindow | grep "workspace: " | cut -d' ' -f2)" == "-99" ]; then
-	waybar &
-	exit
+	coproc waybar $*
+	WayPID=$!
 else
 	kill -9 $WayPID
 	echo "Terminated waybar with PID: $WayPID"
 fi
+
+while true; do
+	echo "LOOPING"
+	if [ "$(hyprctl activewindow | grep "workspace: " | cut -d' ' -f2)" != "-99" ]; then
+		kill -9 $WayPID
+		echo "Terminated waybar with PID: $WayPID"
+		exit
+	fi
+	sleep 0.5
+done
